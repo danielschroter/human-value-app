08a499ca104480438f7297844c4a7c75f920ebf1
Daniel Schroter <daniel@schroter.biz>
2023-06-13 07:08:45 +0200

    Shape adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,9 @@ export default function Home() {
   return (
     <main className={`${poppins.className} overflow-hidden bg-gray-900`}>
       <div className="relative mx-auto min-h-screen w-full max-w-6xl">
-        <div className="absolute top-0 w-64 h-64 bg-indigo-400 rounded-full -left-4 filter blur-3xl opacity-20 sm:opacity-30 animate-blob"></div>
-        <div className="absolute top-0 rounded-full w-72 h-72 bg-pink-300 -right-4 filter blur-3xl opacity-20 sm:opacity-30 animate-blob animation-delay-2000"></div>
-        <div className="absolute rounded-full w-72 h-72 bg-emerald-300 -bottom-8 left-20 filter blur-3xl opacity-20 sm:opacity-30 animate-blob animation-delay-4000"></div>
+        <div className="absolute top-1/2 w-64 h-64 bg-indigo-400 rounded-full -left-4 filter blur-3xl opacity-20 sm:opacity-30 animate-blob"></div>
+        <div className="absolute top-1/2 rounded-full w-72 h-72 bg-pink-300 -right-4 filter blur-3xl opacity-20 sm:opacity-30 animate-blob animation-delay-2000"></div>
+        <div className="absolute rounded-full w-72 h-72 bg-emerald-300 bottom-1/2 left-20 filter blur-3xl opacity-20 sm:opacity-30 animate-blob animation-delay-4000"></div>
         <Hero />
         <Footer />
       </div>
