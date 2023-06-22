c755e8c9d7b927ccfd1a4c45e3a5411dc90c0bef
Daniel Schroter <daniel@schroter.biz>
2023-06-22 10:07:47 +0200

    Meta Tags Added and Prev Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,27 @@ import "./globals.css";
 import QueryProvider from "./components/QueryProvider";
 
 export const metadata = {
-  title: "Human Value Detector",
-  description: "Detect Human Values in Arguments",
+  metadataBase: new URL("https://value-detector.schroter.eu"),
+  title:
+    "Human Value Detector - Competition winning system to discover human values in arguments",
+  description:
+    "Competition winning transformer based system to discover human values in arguments. We presented our system at the ACL 2023 conference and published a system description paper in their proceedings.",
+  authors: [{ name: "Daniel Schroter", url: "https://daniel.schroter.eu" }],
+  keywords: [
+    "human values",
+    "argument mining",
+    "transformer",
+    "competition",
+    "ACL",
+    "2023",
+  ],
+  openGraph: {
+    url: "https://value-detector.schroter.eu",
+    title:
+      "Human Value Detector - Competition winning system to discover human values in arguments",
+    description:
+      "Transformer based system that delivers new state of the art performance to discover human values in arguments.",
+  },
 };
 
 export default function RootLayout({
@@ -14,6 +33,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          property="og:image"
+          itemProp="image"
+          content="https://value-detector.schroter.eu/hero_prev.png"
+        />
+      </head>
       <QueryProvider>
         <body>{children}</body>
       </QueryProvider>
